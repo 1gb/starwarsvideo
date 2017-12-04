1f689eb0731a14e09a106abc7296fd175f54222c
Eve Worthington <worthington.ee@gmail.com>
2017-12-04 12:15:14 -0800

    favicon, video list edits, projector flickering

--- main.js
+++ main.js
@@ -41,24 +41,9 @@ var videos = [
     end: 54
   },
   {
-    id: 'isIKM9_uwpc',
-    start: 14,
-    end: 25
-  },
-  {
     id: 'zRqBpD8M72g',
     start: undefined,
     end: undefined
-  },
-  {
-    id: 'G25zq7y9ylw',
-    start: 80,
-    end: 95
-  },
-  {
-    id: 'gVhHnIrSuTM',
-    start: 0,
-    end: 28
   },
   {
     id: 'eJz-WsDSA0g',
@@ -66,18 +51,13 @@ var videos = [
     end: undefined
   },
   {
-    id: '0pqcE99ViUU',
-    start: 8,
-    end: 14
-  },
-  {
-    id: 'bCJDPd15n0A',
+    id: 'NbDlICh_opI',
     start: undefined,
     end: undefined
   },
   {
-    id: 'ir1jY7m9z6U',
-    start: 9,
+    id: 'bCJDPd15n0A',
+    start: undefined,
     end: undefined
   },
   {
@@ -86,19 +66,9 @@ var videos = [
     end: 20
   },
   {
-    id: '-umJj_jPvlA',
-    start: 1,
-    end: 8
-  },
-  {
     id: '2RG-96tUEFI',
     start: 0,
     end: 10
-  },
-  {
-    id: 'JU5wjnrzIv0',
-    start: 12,
-    end: 23
   },
   {
     id: 'SQZW6Jp5R1A',
@@ -149,16 +119,6 @@ var videos = [
     id: 'lNpIzrGZ5Ow',
     start: 90,
     end: 119
-  },
-  {
-    id: 'gk3Gkqjs-CQ',
-    start: 12,
-    end: 24
-  },
-  {
-    id: 'GW3FzjX6Rwk',
-    start: 0,
-    end: 15
   },
   {
     id: '3lVy5HyjkCc',
@@ -236,11 +196,6 @@ var videos = [
     end: 57
   },
   {
-    id: 'ySJF9D2PdZc',
-    start: undefined,
-    end: undefined
-  },
-  {
     id: 'jL73xcc-DX0',
     start: undefined,
     end: undefined
@@ -254,11 +209,6 @@ var videos = [
     id: 'HMXhqe1Rozk',
     start: 57,
     end: 78
-  },
-  {
-    id: '6_ZS7x83Y60',
-    start: 7,
-    end: 13
   },
   {
     id: 'dKYRqtTdkR0',
@@ -296,19 +246,9 @@ var videos = [
     end: 50
   },
   {
-    id: 'GbPsYuCbvRM',
-    start: 4,
-    end: undefined
-  },
-  {
     id: 'a8Y1w1w_kFM',
     start: 27,
     end: 59
-  },
-  {
-    id: 'l--Ru1Bdvhg',
-    start: 9,
-    end: 36
   },
   {
     id: 'UaDhVHBD_zQ',
@@ -321,42 +261,12 @@ var videos = [
     end: 24
   },
   {
-    id: 'PCKF4BKTClc',
-    start: 3,
-    end: undefined
-  },
-  {
-    id: '1o9IY06Jp90',
-    start: undefined,
-    end: undefined
-  },
-  {
     id: '4Hkp_XG8DX4',
     start: undefined,
     end: undefined
   },
   {
-    id: 'd5ze_7sPM3o',
-    start: 6,
-    end: 32
-  },
-  {
     id: 'Xr9p_2hB7qM',
-    start: undefined,
-    end: undefined
-  },
-  {
-    id: '-XtvopLh5Aw',
-    start: 19,
-    end: 50
-  },
-  {
-    id: '-lKH1ryslZw',
-    start: 0,
-    end: 16
-  },
-  {
-    id: '7SF2BXnqBFc',
     start: undefined,
     end: undefined
   },
@@ -371,16 +281,6 @@ var videos = [
     end: undefined
   },
   {
-    id: '1KLSFNMI1Dg',
-    start: 30,
-    end: 66
-  },
-  {
-    id: 'X4NNf42Ro28',
-    start: 0,
-    end: 24
-  },
-  {
     id: 'UXp5O3-xYrE',
     start: undefined,
     end: undefined
@@ -388,12 +288,12 @@ var videos = [
   {
     id: 'wwGlUIB82Jc',
     start: 0, 
-    end: 9
+    end: 13
   },
   {
     id: 'jC4fACdh3S4',
     start: 0,
-    end: 7
+    end: 10
   },
   {
     id: 'DkXyeMIcKhU',
@@ -403,22 +303,17 @@ var videos = [
   {
     id: 'mLvMJIsMagY',
     start: 0,
-    end: 6
+    end: 10
   },
   {
     id: '-JoLThZsOTY',
     start: 0, 
-    end: 7
+    end: 11
   },
   {
     id: 'V1FnT7MD2cw',
     start: 0,
     end: 26
-  },
-  {
-    id: 'HusZhmugoAg',
-    start: 1,
-    end: 12
   },
   {
     id: 'ABTGPJhwL0Q',
@@ -436,19 +331,14 @@ var videos = [
     end: undefined
   },
   {
-    id: '6pEdiM1F8_8',
-    start: 11,
-    end: 17
-  },
-  {
     id: 'HaWuCjLqtFA',
     start: 70,
     end: 92
   },
   {
     id: 'gZDnaDr0CBg',
-    start: undefined,
-    end: undefined
+    start: 14,
+    end: 29
   },
   {
     id: '1X8k1aT4Cnk',
@@ -461,24 +351,9 @@ var videos = [
     end: 53
   },
   {
-    id: 'VZvHIeMZG_g',
-    start: 3,
-    end: 19
-  },
-  {
-    id: 'BnbNXlpWdh8',
-    start: 0,
-    end: 42
-  },
-  {
     id: 'O2ZPVUO1P9A',
     start: undefined,
     end: undefined
-  },
-  {
-    id: 'kT9m85xjCn4',
-    start: 0,
-    end: 34
   },
   {
     id: 'LpS5qi198v0',
@@ -508,12 +383,7 @@ var videos = [
   {
     id: 'nETugM1lswc',
     start: 2,
-    end: 21
-  },
-  {
-    id: 'dseVIM3bj4',
-    start: 0,
-    end: 21
+    end: 10
   },
   {
     id: 'xohCm3cBJA4',
@@ -521,19 +391,9 @@ var videos = [
     end: undefined
   },
   {
-    id: 'Chc5mSpNgBo',
-    start: undefined,
-    end: undefined
-  },
-  {
     id: 'G5mR-y5vcQw',
     start: undefined,
     end: undefined
-  },
-  {
-    id: 'Zz6Fc_eetnE',
-    start: 0,
-    end: 11
   },
   {
     id: 'B9rKE2QCu58',
@@ -616,6 +476,176 @@ var videos = [
     end: 55
   },
   {
+    id: '0F0OHpEuNUQ',
+    start: 23,
+    end: undefined
+  },
+  {
+    id: 'JhHe57Dn5Rc',
+    start: undefined,
+    end: undefined
+  },
+  {
+    id: 'krb-TPxXj4Y',
+    start: undefined,
+    end: undefined
+  },
+  {
+    id: 'YnzewVuONBs',
+    start: undefined,
+    end: undefined
+  },
+  {
+    id: 'sg9cWg2g0y8',
+    start: undefined,
+    end: undefined
+  },
+  {
+    id: 'Z9zoQj4moo4',
+    start: undefined,
+    end: undefined
+  },
+  {
+    id: 'uYlI7Mymb7U',
+    start: undefined,
+    end: undefined
+  },
+  {
+    id: 'nOQIqwWgy9A',
+    start: 0,
+    end: 19
+  },
+  {
+    id: 'Knc8RpJg8b8',
+    start: 0,
+    end: 13
+  },
+  {
+    id: '2w4fFn5EPms',
+    start: 0,
+    end: 52
+  },
+  {
+    id: 'FjoJZbyNy9g',
+    start: 43,
+    end: 61
+  },
+  {
+    id: 'BeYspXO8Ey8',
+    start: 0,
+    end: 46
+  },
+  {
+    id: 'Ekp7DY1xGzg',
+    start: 36,
+    end: 70
+  },
+  {
+    id: 'crekEen3HvM',
+    start: 20,
+    end: 46
+  },
+  {
+    id: 'mCCgmY0N5sc',
+    start: 0,
+    end: 14
+  },
+  {
+    id: 'vIOfclFzSZY',
+    start: 30,
+    end: 44
+  },
+  {
+    id: '6s9XPu-RT8I',
+    start: 76,
+    end: 103
+  },
+  {
+    id: 'VPfcBuvhF_g',
+    start: 0,
+    end: 15
+  },
+  {
+    id: '_2irWjQo52g',
+    start: undefined,
+    end: undefined
+  },
+  {
+    id: 'EdBLhKMGsx0',
+    start: 30,
+    end: 55
+  },
+  {
+    id: '3MQOQ4qy9_I',
+    start: 29,
+    end: 39
+  },
+  {
+    id: 'Y-fDlHegYo8',
+    start: 0,
+    end: 14
+  },
+  {
+    id: 'cLqnR7uAcnc',
+    start: 27,
+    end: 36
+  },
+  {
+    id: 'xBKA5k9Y-0g',
+    start: 42,
+    end: 50
+  },
+  {
+    id: 'CsuCtEAj5_k',
+    start: 42,
+    end: 63
+  },
+  {
+    id: 'sW0DNuGR4sc',
+    start: undefined,
+    end: undefined
+  },
+  {
+    id: '-A8YxuIwzCo',
+    start: 0,
+    end: 18
+  },
+  {
+    id: 'tl7-QWXdV6A',
+    start: 13,
+    end: 28
+  },
+  {
+    id: 'NEOQkyf5zUk',
+    start: 300,
+    end: 330
+  },
+  {
+    id: 'Uix_CvxNWNM',
+    start: 0,
+    end: 14
+  },
+  {
+    id: '3UFuOUqHFJU',
+    start: 1370,
+    end: 1389
+  },
+  {
+    id: 'L6OMSjpGXkg',
+    start: 14,
+    end: 29
+  },
+  {
+    id: 'KjbVM06OTQY',
+    start: undefined,
+    end: undefined
+  },
+  {
+    id: 'UMKuFqB3RwA',
+    start: 15,
+    end: 26
+  },
+  {
     id: '6cPrEwhESoA',
     start: 94,
     end: 118
@@ -630,9 +660,13 @@ $( document ).ready(function() {
   var player;
   window.onYouTubeIframeAPIReady = function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
+      playerVars: {
+        modestbranding: 1
+      },
       events: {
         'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
+        'onStateChange': onPlayerStateChange,
+        'onError': onPlayerError
       }
     });
   };
@@ -642,10 +676,21 @@ $( document ).ready(function() {
     return 0.5 - Math.random();
   });
   
+  var areAnyDuplicates = false;
+
+  videos.map(function(obj) {
+      return obj.id;
+  }).forEach(function (element, index, arr) {
+      if (arr.indexOf(element) !== index) {
+          areAnyDuplicates = true;
+      }
+  });
+  
+  // console.log(areAnyDuplicates);
   
   videos.forEach(function(object) {
     object.thumbnailUrl = 'https://i.ytimg.com/vi/' + object.id + '/mqdefault.jpg';
-    object.videoUrl = 'https://www.youtube.com/embed/' + object.id + '?enablejsapi=1&controls=0&showinfo=0&rel=0&modestbranding=1'; //?rel=0&amp;controls=0&amp;autoplay=1&amp;showinfo=0';  
+    object.videoUrl = 'https://www.youtube.com/embed/' + object.id + '?enablejsapi=1&showinfo=0&rel=0'; //?rel=0&amp;controls=0&amp;autoplay=1&amp;showinfo=0';  
     if (object.start !== undefined) {
       object.videoUrl += ('&amp;start=' + object.start);
     }
@@ -671,6 +716,11 @@ $( document ).ready(function() {
          });
     $('#' + videos[0].id).addClass('glow');
     nextVideo = videos[1].id;
+  }
+  
+  // On error (video deleted), play next
+  function onPlayerError() {
+    playCurrentVideo(nextVideo);
   }
   
   function changeBorderColor(playerStatus) {
@@ -710,16 +760,17 @@ $( document ).ready(function() {
         player.loadVideoById({'videoId': id,
                'startSeconds': start,
                'endSeconds': end
-             });
-             
-             // Mysterious stopping?
-             
+             });             
         $('.thumbnail').removeClass('glow');
         $('#' + id).addClass('glow');
         nextVideo = videos[i + 1].id;
       }
     }    
   }
+  
+  $('#nextbtn').on('click', function() {
+    playCurrentVideo(nextVideo);
+  });
   
   // Make thumbnails
   // videos.forEach(function(object) {
@@ -735,7 +786,12 @@ $( document ).ready(function() {
 }); //End ready
 
 // To do:
-// - change origin
-// - style better
-// - add other videos
-// - debug mysterious stopping/skipping
+// - change origin... to github? to starwars.video? Maybe ask 
+// - add meta data descriptions
+// - style better... new ship! add ottom line to SVG
+// add holographic projection
+// add list of all videos ... modal? 
+// - clean up code... i-frame-demo?
+
+// timing wrong on star wars bottle flip, ends too late
+// first go at projector lights okay, next try chopping up a triangle
